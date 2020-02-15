ae930c7796b812a4fc7f14e9f376779bf21c2e37
Bo Henderson <bohende@gmail.com>
2020-02-15 16:23:45 +0530

    🐛 server: remove doubled up slashes from content url

--- modules/server/src/content.ts
+++ modules/server/src/content.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import path from 'path';
-import fetch from 'node-fetch';
+import express from "express";
+import path from "path";
+import fetch from "node-fetch";
 
-import { env } from './env';
+import { env } from "./env";
 
-export const content = express.Router()
+export const content = express.Router();
 
 content.use(async (req, res, next): Promise<void> => {
   if (!env.contentUrl || env.devMode) {
     next();
     return;
   }
-  const url = `${env.contentUrl}/${req.path.replace(/^\/content\//, '')}`
-  let result
+  const url = `${env.contentUrl}/${req.path.replace(/^\/content\//, "")}`.replace(/[^:]\/+/, "/");
+  let result;
   try {
-    const response = await fetch(url)
+    const response = await fetch(url);
     if (response.status === 200) {
       console.log(`Forwarding content from ${url}`);
-      url.endsWith('.json')
+      url.endsWith(".json")
         ? res.json(await response.json())
         : res.send(await response.text());
       return;
@@ -31,9 +31,9 @@ content.use(async (req, res, next): Promise<void> => {
 
 // Fallback to static copy of content (maybe out-of-date)
 content.use(async (req, res, next): Promise<void> => {
-  const folder = path.normalize('/blog-content')
+  const folder = path.normalize("/blog-content");
   console.log(`Attempting to get static file ${folder}${req.path}`);
   express.static(folder, {
-    extensions: ['json', 'md'],
+    extensions: ["json", "md"],
   } )(req, res, next);
-})
+});
