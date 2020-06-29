023176e462e9b6da54f3febfa07ea35953a20aef
shivani <gupta.shivani254@gmail.com>
2020-06-29 20:18:28 +0530

    🍽  init add meal dialog

---
+++ modules/client/src/components/AddMeal.tsx
@@ -0,0 +1,45 @@
+import React, { useState, } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  IconButton,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import {
+  Close as CloseIcon,
+  SaveAlt as SaveIcon,
+} from "@material-ui/icons";
+
+import { DateTime } from "./DateTimePicker"
+import { TransferList } from "./TransferList";
+
+export const AddMeal = (props: any) => {
+  const {
+    dialog,
+    handleProfileSave,
+    toggleMealDialog,
+  } = props;
+
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Dialog open={dialog} onClose={toggleMealDialog}>
+      <DialogContent>
+        <DateTime date={date} label="Meal Time" setDate={setDate}/>
+        <Typography> Dishes </Typography>
+        <TransferList />
+      </DialogContent>
+      <DialogActions>
+        <IconButton onClick={handleProfileSave}>
+          <SaveIcon />
+        </IconButton>
+        <IconButton onClick={toggleMealDialog}>
+          <CloseIcon />
+        </IconButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
--- modules/client/src/components/FoodLog.tsx
+++ modules/client/src/components/FoodLog.tsx
@@ -19,25 +19,40 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import {
+  AddCircle as AddIcon,
   Close as CloseIcon,
   Edit as EditIcon,
   SaveAlt as SaveIcon,
 } from "@material-ui/icons";
 
-import { ProfileEdit } from "./ProfileEdit";
 import { store } from "../utils/cache";
+import { AddMeal } from "./AddMeal";
 import { emptyFoodLog } from "../utils/constants";
 
 export const FoodLog = (props: any) => {
 
-  const { foodLog } = props;
+  const { foodLog, handleProfileSave } = props;
+
+  const [dialog, setDialog] = useState(false);
+
+  const toggleMealDialog = () => setDialog(!dialog);
 
   let msg = "Here is your food log";
   if (JSON.stringify(foodLog) === JSON.stringify(emptyFoodLog))
     msg = "You have no meal entry yet!!";
+
   return (
     <>
       {msg} 
+      <IconButton onClick={toggleMealDialog}>
+        <AddIcon />
+      </IconButton>
+      <AddMeal
+        dialog={dialog}
+        foodLog={foodLog}
+        handleProfileSave={handleProfileSave}
+        toggleMealDialog={toggleMealDialog}
+      />
     </>
   )
 }
