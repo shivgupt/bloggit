461712ff49308b0892de767cccbbc03a75bb2a2f
shivani <gupta.shivani254@gmail.com>
2020-07-03 19:54:50 +0530

    💄 add meal ui

--- modules/client/src/components/AddMeal.tsx
+++ modules/client/src/components/AddMeal.tsx
@@ -23,7 +23,8 @@ import { DateTime } from "./DateTimePicker";
 import { NutritionInfo } from "./NutritionInfo";
 
 import { Dish } from "../types";
-import { Dishes } from "../utils/dishes";
+// import { Dishes } from "../utils/dishes";
+import * as Dishes from "../utils/dishes";
 
 import { store } from "../utils/cache";
 import { dateOptions, timeOptions, emptyDish } from "../utils/constants";
@@ -42,8 +43,8 @@ export const AddMeal = (props: any) => {
     msg: "",
   });
   const [mealTime, setMealTime] = useState(new Date());
-  const [selected, setSelected] = React.useState<Dish[]>([]);
-  const [dishOptions, setDishOptions] = React.useState<Dish[]>(Dishes);
+  const [selected, setSelected] = React.useState<any>({});
+  const [dishOptions, setDishOptions] = React.useState<any>(Dishes);
   const [dishOptionsView, setDishOptionsView] = useState(false);
   const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
   const [infoDialog, setInfoDialog] = React.useState(false);
@@ -55,28 +56,32 @@ export const AddMeal = (props: any) => {
 
   const toggleInfoDialog = () => setInfoDialog(!infoDialog);
 
-  const handleInfo = (dish: Dish) => () => {
-    setSelectedDish(dish);
+  const handleInfo = (dish: string) => () => {
+    setSelectedDish(Dishes[dish]);
     setInfoDialog(true);
   };
 
-  const handleToggle = (dish: Dish) => () => {
-    const optionsIndex = dishOptions.indexOf(dish);
-    const selectedIndex = selected.indexOf(dish);
-    const newOptions = [ ...dishOptions ];
-    const newSelected = [ ...selected ];
-
-    if (optionsIndex === -1) {
-      newOptions.push(dish);
-      newSelected.splice(selectedIndex, 1);
-    } else {
-      newOptions.splice(optionsIndex, 1);
-      newSelected.push(dish);
+  const handleAdd = (dish: string) => () => {
+    const newSelected = { ...selected };
+    try {
+      newSelected[dish].serving += 1;
+    } catch {
+      newSelected[dish] = { ...Dishes[dish] };
     }
-
     setSelected(newSelected);
-    setDishOptions(newOptions);
-  }
+  };
+
+  const handleDelete = (dish: string) => () => {
+    const newSelected = { ...selected };
+    try {
+      newSelected[dish].serving -= 1;
+      if (newSelected[dish].serving === 0)
+        delete newSelected[dish];
+    } catch {
+      console.log(dish);
+    }
+    setSelected(newSelected);
+  };
 
   const toggleDishOptionsView = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -84,10 +89,15 @@ export const AddMeal = (props: any) => {
   }
 
   const handleAddMeal = () => {
-    if (selected.length === 0) {
+    if (Object.keys(selected).length === 0) {
       setMealEntryAlert({ severity: "error", msg: "You have not selected any dish! What did you eat in your meal?" });
       return;
     }
+
+    const meal = [] as Dish[];
+    Object.values(selected).forEach((dish) => meal.push(dish as Dish));
+    console.log(selected);
+    console.log(meal);
 
     const date = mealTime.toLocaleDateString([], dateOptions);
     const time = mealTime.toLocaleTimeString([], timeOptions);
@@ -97,13 +107,13 @@ export const AddMeal = (props: any) => {
     if (newFoodLog[date]) {
       if (newFoodLog[time]) {
         console.log("found date and time concatinating");
-        newFoodLog[date][time].concat(selected);
+        newFoodLog[date][time].concat(meal);
       } else {
         console.log("found date adding time");
-        newFoodLog[date][time] = selected;
+        newFoodLog[date][time] = meal;
       }
     } else {
-      newFoodLog[date] = { [time]: selected };
+      newFoodLog[date] = { [time]: meal };
     }
 
     setFoodLog(newFoodLog);
@@ -142,12 +152,12 @@ export const AddMeal = (props: any) => {
             <FoodIcon />
           </IconButton>
           <List>
-            {selected.map((dish: Dish) => (
-              <ListItem key={dish.name} role="listitem" button onClick={handleToggle(dish)}>
+            {Object.keys(selected).map((dish: string) => (
+              <ListItem key={dish} role="listitem" button onClick={handleDelete(dish)}>
                 <Chip
                   color="secondary"
-                  label={dish.name}
-                  onDelete={handleToggle(dish)}
+                  label={dish}
+                  onDelete={handleDelete(dish)}
                 />
               </ListItem>
             ))}
@@ -155,11 +165,11 @@ export const AddMeal = (props: any) => {
           <Popover id="dish-options-menu" anchorEl={anchorEl} open={dishOptionsView} onClose={toggleDishOptionsView}>
             <Paper>
               <label> Dish Options </label>
-              {dishOptions.map((dish) => (
-                <ListItem key={dish.name} role="listitem" button onClick={handleToggle(dish)}>
+              {Object.keys(dishOptions).map((dish: string) => (
+                <ListItem key={dish} role="listitem" button onClick={handleAdd(dish)}>
                   <Chip
                     color="secondary"
-                    label={dish.name}
+                    label={dish}
                     onDelete={handleInfo(dish)}
                     deleteIcon={<InfoIcon />}
                   />
