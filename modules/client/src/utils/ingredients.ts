6c449f9cb7bb073cdd8185cfa09cbecc82ea92ab
shivani <gupta.shivani254@gmail.com>
2020-07-17 17:27:55 +0530

    🍕 add calories to ingredients

--- modules/client/src/utils/ingredients.ts
+++ modules/client/src/utils/ingredients.ts
@@ -4,6 +4,7 @@ export const Almond = {
   name: "Almond",
   quantity: "100",
   nutrients: {
+    calories: 579,
     carbohydrates: 22,
     protein: 21,
     fat: 50,
@@ -14,9 +15,10 @@ export const Broccoli = {
   name: "Broccoli",
   quantity: "100",
   nutrients: {
-    carbohydrates: 6,
-    protein: 2.5,
-    fat: 0.3,
+    calories: 34,
+    carbohydrates: 6.6,
+    protein: 2.8,
+    fat: 0.4,
   },
 } as Ingredient;
 
@@ -24,6 +26,7 @@ export const Caschew = {
   name: "Caschew",
   quantity: "100",
   nutrients: {
+    calories: 553,
     carbohydrates: 30,
     protein: 18,
     fat: 44,
@@ -34,9 +37,43 @@ export const Cheese = {
   name: "Cheese",
   quantity: "100",
   nutrients: {
-    carbohydrates: 3.7,
-    protein: 18,
-    fat: 32,
+    calories: 356,
+    carbohydrates: 2.2,
+    protein: 25,
+    fat: 27,
+  },
+} as Ingredient;
+
+export const Cherry = {
+  name: "Cherry",
+  quantity: "100",
+  nutrients: {
+    calories: 63,
+    carbohydrates: 16,
+    protein: 1.1,
+    fat: 0.2,
+  },
+} as Ingredient;
+
+export const CoconutOil = {
+  name: "CoconutOil",
+  quantity: "100",
+  nutrients: {
+    calories: 892,
+    carbohydrates: 0,
+    protein: 0,
+    fat: 100,
+  },
+} as Ingredient;
+
+export const DoubleChocolateProtein = {
+  name: "DoubleChocolateProtein",
+  quantity: "30",
+  nutrients: {
+    calories: 117,
+    carbohydrates: 1,
+    protein: 80,
+    fat: 0.33,
   },
 } as Ingredient;
 
@@ -55,70 +92,10 @@ export const Egg = {
   name: "Egg",
   quantity: "100",
   nutrients: {
+    calories: 143,
     carbohydrates: 1.1,
     protein: 13,
     fat: 11,
-  },
-} as Ingredient;
-
-export const WholeMilk = {
-  name: "Whole Milk",
-  quantity: "100",
-  nutrients: {
-    carbohydrates: 4.8,
-    protein: 3.2,
-    fat: 3.3,
-  },
-} as Ingredient;
-
-export const Onion = {
-  name: "Onion",
-  quantity: "100",
-  nutrients: {
-    carbohydrates: 9.3,
-    protein: 1.1,
-    fat: 0.1,
-  },
-} as Ingredient;
-
-export const Tomato = {
-  name: "Tomato",
-  quantity: "100",
-  nutrients: {
-    carbohydrates: 3.9,
-    protein: 0.9,
-    fat: 0.2,
-  },
-} as Ingredient;
-
-export const CoconutOil = {
-  name: "CoconutOil",
-  quantity: "100",
-  nutrients: {
-    carbohydrates: 0,
-    protein: 0,
-    fat: 100,
-  },
-} as Ingredient;
-
-export const DoubleChocolateProtein = {
-  name: "DoubleChocolateProtein",
-  quantity: "30",
-  nutrients: {
-    calories: 117,
-    carbohydrates: 1,
-    protein: 80,
-    fat: 0.33,
-  },
-} as Ingredient;
-
-export const Cherry = {
-  name: "Cherry",
-  quantity: "100",
-  nutrients: {
-    carbohydrates: 12,
-    protein: 1,
-    fat: 0.3,
   },
 } as Ingredient;
 
@@ -126,9 +103,32 @@ export const Litchi = {
   name: "Litchi",
   quantity: "50",
   nutrients: {
+    calories: 66,
     carbohydrates: 17,
     protein: 0.8,
     fat: 0.4,
+  },
+} as Ingredient;
+
+export const Oats = {
+  name: "Oats",
+  quantity: "100",
+  nutrients: {
+    calories: 246,
+    carbohydrates: 66,
+    protein: 17,
+    fat: 7,
+  },
+} as Ingredient;
+
+export const Onion = {
+  name: "Onion",
+  quantity: "100",
+  nutrients: {
+    calories: 40,
+    carbohydrates: 9.3,
+    protein: 1.1,
+    fat: 0.1,
   },
 } as Ingredient;
 
@@ -143,12 +143,24 @@ export const RefinedFlour = {
   },
 } as Ingredient;
 
-export const Oats = {
-  name: "Oats",
+export const Tomato = {
+  name: "Tomato",
   quantity: "100",
   nutrients: {
-    carbohydrates: 66,
-    protein: 14.2,
-    fat: 8.5,
+    calories: 16,
+    carbohydrates: 3.2,
+    protein: 1.2,
+    fat: 0.2,
+  },
+} as Ingredient;
+
+export const WholeMilk = {
+  name: "Whole Milk",
+  quantity: "100",
+  nutrients: {
+    calories: 62,
+    carbohydrates: 4.9,
+    protein: 3.2,
+    fat: 3.3,
   },
 } as Ingredient;
