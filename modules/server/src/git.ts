9d7d985339f122c89d0da622718b78d04cba0325
bohendo <bohendo@pm.me>
2021-02-07 15:35:56 +0530

    implement git endpoint for fetching file contents

--- modules/server/src/git.ts
+++ modules/server/src/git.ts
@@ -3,27 +3,61 @@ import path from "path";
 
 import express from "express";
 import git from "isomorphic-git";
-import http from "isomorphic-git/http/node";
-
-import { env } from "./env";
 
 export const gitRouter = express.Router();
 
-git.clone({
-  fs,
-  http,
-  dir: path.normalize("/blog-content"),
-  url: env.contentRepo,
-})
-  .then(res => console.log(`Git clone result: ${res}`))
-  .catch(e => console.log(`Git clone error: ${e}`));
+const gitOpts = { fs, dir: path.normalize("/blog-content") };
 
-gitRouter.get("/", (req, res): void => {
-  console.log(`git getting path ${req.path}`);
-  res.json({ status: "git is ok" });
-});
+gitRouter.get("/:ref/:category/:file", async (req, res, next): Promise<void> => {
+  const { ref: givenRef, category, file } = req.params;
+  const filepath = `${category}/${file}`;
+  let ref;
+  try {
+    console.log(`Trying to expand given oid: ${givenRef}`);
+    ref = await git.expandOid({ ...gitOpts, oid: givenRef });
+  } catch (e) {
+    console.log(`Failed to expand oid ${givenRef}: ${e.message}`);
+    try {
+      console.log(`Trying to expand given ref: ${givenRef}`);
+      ref = await git.expandRef({ ...gitOpts, ref: givenRef });
+    } catch (e) {
+      console.log(`Failed to expand ref ${givenRef}: ${e.message}`);
+    }
+  }
+  // does this ref exist?
+  try {
+    await git.checkout({
+      ...gitOpts,
+      dryRun: true,
+      force: true,
+      ref,
+    });
+  } catch (e) {
+    console.log(`Cannot find ref ${ref}: ${e.message}`);
+    return next();
+  }
 
-gitRouter.get("/main", (req, res): void => {
-  console.log(`git getting main branch file at ${req.path}`);
-  res.json({ status: "git is at main" });
+  console.log(`git checking out ref ${ref}`);
+  // checkout this ref
+  await git.checkout({
+    ...gitOpts,
+    force: true,
+    ref,
+  });
+
+  try {
+    const blob = await git.readBlob({
+      ...gitOpts,
+      oid: ref,
+      filepath,
+    });
+    console.log(`got a blob object w props: ${Object.keys(blob)}`);
+    const parsedBlob = Buffer.from(blob.blob).toString("utf8");
+    res.status(200).send(parsedBlob);
+
+  } catch (e) {
+    console.log(`Failed to read object w oid ${ref} and filepath ${filepath}: ${e.message}`);
+    next();
+  }
+
 });
