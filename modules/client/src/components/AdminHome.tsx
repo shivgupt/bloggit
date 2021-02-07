67a7cd84dfb107042760e20d0334ddb483f66947
shivani <gupta.shivani254@gmail.com>
2021-02-07 16:20:21 +0530

    ✨ init key update

--- modules/client/src/components/AdminHome.tsx
+++ modules/client/src/components/AdminHome.tsx
@@ -13,44 +13,54 @@ export const AdminHome = (props: any) => {
   const adminContext = useContext(AdminContext);
 
   const handleRegister = () => {
-    const keyId = (document.getElementById("key-id") as HTMLInputElement).value;
-    const keyValue = (document.getElementById("key-value") as HTMLInputElement).value;
-    console.log(keyId);
+    const id = (document.getElementById("key-id") as HTMLInputElement).value;
+    const value = (document.getElementById("key-value") as HTMLInputElement).value;
+    console.log(adminContext.updateKey);
+    adminContext.updateKey({id, value});
   }
   return (
     <div>
       
       <Typography variant="h4"> Admin Key</Typography>
       {adminContext.key && adminContext.key.id
-        ? <Typography variant="h6">{adminContext.key.id}</Typography>
-        : <Typography variant="subtitle1"> You have no key registered on this device for Admin access</Typography>
+        ? (
+          <>
+            <Typography variant="h6">{adminContext.key.id}</Typography>
+            <Typography variant="h6">{adminContext.key.value}</Typography>
+          </>
+        )
+        : (
+          <>
+            <Typography variant="subtitle1">
+              You have no key registered on this device for Admin access
+            </Typography>
+            <Typography variant="h6">Use authorized key to register new key</Typography>
+
+            &nbsp;
+
+            <TextField
+              id="key-id"
+              label="Key ID"
+              placeholder="mobile-key"
+              defaultValue={""}
+            />
+
+            <TextField
+              id="key-value"
+              label="(NEW) Public Key"
+              placeholder="mobile-pubKey-value"
+              defaultValue={""}
+            />
+
+            <TextField
+              id="authorized-key"
+              label="Authorized Private Key"
+              placeholder="authorized-privKey-value"
+            />
+            <Button onClick={handleRegister}> Register </Button>
+          </>
+        )
       }
-
-      <Typography variant="h5">Register New Key</Typography>
-      <Typography variant="h6">Use authorized key to register new key</Typography>
-
-      &nbsp;
-
-      <TextField
-        id="key-id"
-        label="Key ID"
-        placeholder="mobile-key"
-        defaultValue={""}
-      />
-
-      <TextField
-        id="key-value"
-        label="(NEW) Public Key"
-        placeholder="mobile-pubKey-value"
-        defaultValue={""}
-      />
-
-      <TextField
-        id="authorized-key"
-        label="Authorized Private Key"
-        placeholder="authorized-privKey-value"
-      />
-      <Button onClick={handleRegister}> Register </Button>
 
     </div>
   )
