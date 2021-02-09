f8421c3dc6e46e568eb430c5acca29a2b968cdf7
shivani <gupta.shivani254@gmail.com>
2021-02-09 09:11:50 +0530

    💄 add styles to admin home

--- modules/client/src/components/AdminHome.tsx
+++ modules/client/src/components/AdminHome.tsx
@@ -1,16 +1,27 @@
 import React, { useContext } from "react";
 import { 
   Button,
-  Checkbox,
+  Divider,
+  makeStyles,
   TextField,
+  Theme,
   Typography,
  } from "@material-ui/core";
 
 import { AdminContext } from "../AdminContext";
 
+const useStyles = makeStyles((theme: Theme) => ({
+  section: {
+    margin: theme.spacing(3, 2),
+    '& > *': {
+      margin: theme.spacing(1),
+    }
+  },
+}))
 export const AdminHome = (props: any) => {
 
   const adminContext = useContext(AdminContext);
+  const classes = useStyles();
 
   const handleRegister = () => {
     const id = (document.getElementById("key-id") as HTMLInputElement).value;
@@ -24,43 +35,57 @@ export const AdminHome = (props: any) => {
       <Typography variant="h4"> Admin Key</Typography>
       {adminContext.key && adminContext.key.id
         ? (
-          <>
-            <Typography variant="h6">{adminContext.key.id}</Typography>
-            <Typography variant="h6">{adminContext.key.value}</Typography>
-          </>
+          <div className={classes.section}>
+            <TextField
+              disabled
+              id="key-id-registered"
+              label="Key ID"
+              variant="outlined"
+              value={adminContext.key.id}
+            />
+            <TextField
+              disabled
+              id="key-value-registered"
+              label="Key Value"
+              variant="outlined"
+              value={adminContext.key.value}
+            />
+          </div>
         )
         : (
-          <>
+          <div className={classes.section}>
             <Typography variant="subtitle1">
               You have no key registered on this device for Admin access
             </Typography>
-            <Typography variant="h6">Use authorized key to register new key</Typography>
-
-            &nbsp;
-
-            <TextField
-              id="key-id"
-              label="Key ID"
-              placeholder="mobile-key"
-              defaultValue={""}
-            />
-
-            <TextField
-              id="key-value"
-              label="(NEW) Public Key"
-              placeholder="mobile-pubKey-value"
-              defaultValue={""}
-            />
-
-            <TextField
-              id="authorized-key"
-              label="Authorized Private Key"
-              placeholder="authorized-privKey-value"
-            />
-            <Button onClick={handleRegister}> Register </Button>
-          </>
+          </div>
         )
       }
+
+      <Divider variant="middle" />
+      <div className={classes.section}>
+        <Typography variant="h6">Use auth token to register new key for this device</Typography>
+
+        <TextField
+          id="key-id"
+          label="Key ID"
+          placeholder="mobile-key"
+          defaultValue={""}
+        />
+
+        <TextField
+          id="key-value"
+          label="(NEW) Public Key"
+          placeholder="mobile-key-value"
+          defaultValue={""}
+        />
+
+        <TextField
+          id="authorized-key"
+          label="Auth Token"
+          placeholder="authorized-token"
+        />
+        <Button onClick={handleRegister}> Register </Button>
+      </div>
 
     </div>
   )
