8ad6c756338b3781d27adeb98519a73fa5d69a8d
shivani <gupta.shivani254@gmail.com>
2021-02-09 14:26:10 +0530

    🐛 fix codeblock scroll

--- modules/client/src/components/CodeBlock.tsx
+++ modules/client/src/components/CodeBlock.tsx
@@ -1,38 +1,22 @@
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  Theme,
-  createStyles,
-  makeStyles,
-} from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
-
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  table: {
-    minWidth: 400,
-  },
-}));
+import { atomDark, vs } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export const CodeBlockRenderer = (props: any) => {
-  const classes = useStyles();
+  const theme = useTheme();
+
+ if (theme.palette.type === "dark") {
+    return (
+      <SyntaxHighlighter showLineNumbers language={props.language} style={atomDark}>
+        {props.value}
+      </SyntaxHighlighter>
+    );
+ }
+
   return (
-    <Paper variant="outlined">
-      <Table className={classes.table} size="small" aria-label="a dense table">
-        <TableBody>
-          <TableRow>
-            <TableCell>
-              <SyntaxHighlighter language={props.language} style={coy}>
-                {props.value}
-              </SyntaxHighlighter>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </Paper>
+    <SyntaxHighlighter language={props.language} style={vs}>
+      {props.value}
+    </SyntaxHighlighter>
   );
 };
