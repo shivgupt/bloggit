daa183a70f6637af8e83b439643841b2810812b8
shivani <gupta.shivani254@gmail.com>
2021-02-13 15:35:12 +0530

    ✨ send update git request

--- modules/client/src/components/Posts.tsx
+++ modules/client/src/components/Posts.tsx
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core";
 import {
   Edit,
+  RestaurantRounded,
   Save,
 } from "@material-ui/icons";
 import React, { useContext, useEffect, useState } from "react";
@@ -15,6 +16,7 @@ import Markdown from "react-markdown";
 import emoji from "emoji-dictionary";
 import ReactMde from "react-mde";
 import "react-mde/lib/styles/css/react-mde-all.css";
+import axios from "axios";
 
 import { AdminContext } from "../AdminContext";
 import { PostData } from "../types";
@@ -42,6 +44,10 @@ export const PostPage = (props: { post?: PostData | string }) => {
   const [newContent, setNewContent] = useState("Loading Page");
   const [content, setContent] = useState("Loading Page");
   const [selectedTab, setSelectedTab] = React.useState<"write" | "preview">("write");
+  
+  useEffect(() => {
+    axios.defaults.headers.common["admin-token"] = adminContext.authToken;
+  }, [adminContext]);
 
   useEffect(() => {
     if (typeof(post) === "string") {
@@ -54,8 +60,29 @@ export const PostPage = (props: { post?: PostData | string }) => {
     }
   },[post]);
 
-  const commitAndPush = () => {
+  const commitAndPush = async () => {
+    if (content === newContent){
+      console.log("no changes detected");
+      return;
+    }
     console.log("Lets push it to git");
+    let path: string;
+    if (typeof(post) === "string") {
+      path = post;
+    } else if (post && post.path) {
+      path = post.path;
+    } else {
+      console.log("error: no path found");
+      return;
+    }
+    let res = await axios({
+      method: "post",
+      url: `git/push/${path}`,
+      data: newContent,
+      headers: { "content-type": "text/plain" }
+    });
+    console.log(res.status);
+    console.log(res);
   }
 
   const emojiSupport = text =>
@@ -75,7 +102,6 @@ export const PostPage = (props: { post?: PostData | string }) => {
     return (<Link color="secondary" href={props.href}> {props.children[0].props.value} </Link>);
   };
 
-  console.log(editMode);
   return (
     <Paper variant="outlined">
       {adminContext.adminMode ?
