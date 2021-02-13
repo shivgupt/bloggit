5a985d314e69f1ea2f816f1df908c676d6316f5f
bohendo <bohendo@pm.me>
2021-02-13 14:51:29 +0530

    init dummy git/push endpoint

--- modules/server/src/git.ts
+++ modules/server/src/git.ts
@@ -27,6 +27,7 @@ const resolveRef = async (givenRef: string): Promise<string> => {
   return ref;
 };
 
+gitRouter.use(bodyParser.text({ type: ["text/plain"] }));
 gitRouter.use(bodyParser.raw({ type: [
   "application/x-git-receive-pack-request",
   "application/x-git-upload-pack-request",
@@ -68,14 +69,44 @@ gitRouter.post([
 // based on https://stackoverflow.com/a/25556917
 // TODO: lock so simultaneous pushes proceed serially
 gitRouter.post("/push/*", async (req, res, _): Promise<void> => {
+  const err = (e: string): void => {
+    log.warn(`Git push failure: ${e}`);
+    res.status(500).send(e);
+    return;
+  };
+  if (!req.body) err("Body Required");
+
   const filepath = req.path.replace(`/push/`, "");
   log.info(`Processing git push for file ${filepath}`);
+  const newBlob = await git.hashBlob({ object: req.body });
+  log.info(`Creating blob from body: ${newBlob.oid}`);
   const latestCommit = await resolveRef(env.defaultBranch);
-  log.info(`latestCommit: ${latestCommit}`);
   // Reset this filepath
   await git.resetIndex({ ...gitOpts, filepath });
-  const indexTree = await git.readTree({ ...gitOpts, oid: latestCommit });
-  log.info(indexTree, "indexTree:");
+  const tree = (await git.readTree({ ...gitOpts, oid: latestCommit })).tree;
+  log.info(tree, "tree:");
+  const parts = filepath.split("/");
+  const filename = parts[parts.length - 1];
+  const subTrees = [];
+  for (const part of parts) {
+    if (part === filename) {
+      // If we needed to add new folders
+      if (subTrees.length > 0) {
+        log.info(`Creating new folders..`);
+      } else {
+        log.info(`Adding file..`);
+      }
+    } else {
+      const node = tree.find(element => element.type === "tree" && element.path === part);
+      if (node) {
+        log.info(node, `Found node in root tree:`);
+      } else {
+        log.info(`Node for ${part} does not exist in root tree`);
+        subTrees.push({ mode: "040000", path: part, oid: "", type: "tree" });
+      }
+    }
+  }
+  // git.writeTree({ ...gitOpts, tree });
   res.json({
     defaultBranch: env.defaultBranch,
   });
