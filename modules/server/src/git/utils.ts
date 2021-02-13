349a9c9c538af59483ff5a57be8a7b41cbec4734
bohendo <bohendo@pm.me>
2021-02-13 16:55:12 +0530

    implement first draft of git push logic

--- modules/server/src/git/utils.ts
+++ modules/server/src/git/utils.ts
@@ -6,6 +6,7 @@ import git from "isomorphic-git";
 import { env } from "../env";
 
 const gitdir = path.normalize(env.contentDir);
+
 export const gitOpts = { fs, dir: gitdir, gitdir };
 
 // Given a branch name or abreviated commit hash, return the full commit hash
@@ -17,4 +18,14 @@ export const resolveRef = async (givenRef: string): Promise<string> => {
     ref = await git.expandOid({ ...gitOpts, oid: givenRef });
   }
   return ref;
+};
+
+//https://stackoverflow.com/a/43934805
+export const strToArray = (str: string): Uint8Array => {
+  const utf8Encoded = unescape(encodeURIComponent(str));
+  return new Uint8Array(utf8Encoded.split("").map(c => c.charCodeAt(0)));
+};
+export const arrToString = (arr: Uint8Array): string => {
+  const utf8String = Array.from(arr).map(item => String.fromCharCode(item)).join("");
+  return decodeURIComponent(escape(utf8String));
 };
