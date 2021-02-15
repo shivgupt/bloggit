c7f513ca5394857fa077ba7beca22c4002967b1c
bohendo <bohendo@pm.me>
2021-02-15 14:05:51 +0530

    dissolve execPackService into git/backend

--- modules/server/src/git/backend.ts
+++ modules/server/src/git/backend.ts
@@ -1,55 +1,78 @@
+import { spawn } from "child_process";
+
+import { env } from "../env";
 import { logger } from "../utils";
 
-import { getGitBackend } from "./bin";
+import { bufferToStream, streamToBuffer } from "./utils";
 
 const log = logger.child({ module: "GitRouter" });
 
-export const getRefInfo = async (req, res, _): Promise<void> => {
-  const err = (e: string): void => {
-    log.warn(`Git backend failure: ${e}`);
-    res.status(500).send(e);
-    return;
+const regex = {
+  "git-receive-pack": RegExp("([0-9a-fA-F]+) ([0-9a-fA-F]+) (refs/[^ \x00]+)( |00|\x00)|^(0000)$"),
+  "git-upload-pack": /^\S+ ([0-9a-fA-F]+)/,
+};
+
+const execPackService = async (
+  service: "git-upload-pack" | "git-receive-pack",
+  payload?: Buffer,
+): Promise<Buffer> => new Promise((resolve, reject) => {
+  let output = Buffer.from([]);
+  const args = [ "--stateless-rpc" ];
+  if (!payload) args.push("--advertise-refs");
+  args.push(env.contentDir);
+  const spawnChild = async (): Promise<void> => {
+    log.info(`Spawning: ${service} ${args.toString().split(",").join(" ")}`);
+    const ps = spawn(service, args);
+    ps.on("error", reject);
+    ps.on("close", code => log.debug(`${service} exited with code ${code}`));
+    ps.stdout.on("data", out => log.debug(`${service} sent ${out.length} chars to stdout`));
+    ps.stderr.on("data", d => log.error(d));
+    ps.stdin.on("data", out => log.debug(`${service} received ${out.length} chars from stdin`));
+    if (payload) bufferToStream(payload).pipe(ps.stdin);
+    return resolve(Buffer.concat([output, await streamToBuffer(ps.stdout)]));
   };
+  if (payload) {
+    if (!regex[service].exec(payload.slice(0,512).toString("utf8"))) {
+      return reject(new Error(`Invalid payload provided to ${service}`));
+    }
+  } else {
+    const flag = "# service=" + service + "\n";
+    const n = (4 + flag.length).toString(16);
+    const infoData = Array(4 - n.length + 1).join("0") + n + flag + "0000";
+    output = Buffer.concat([output, Buffer.from(infoData)]);
+    log.debug(`Adding ${output.length} bytes of ref info data to output`);
+  }
+  spawnChild();
+});
+
+const getErrHandler = res => (e: string): void => {
+  log.error(`Git pack service failure: ${e}`);
+  return res.status(500).send(`Server Error`);
+};
+
+export const getRefInfo = async (req, res, _): Promise<void> => {
+  const err = getErrHandler(res);
   const service = req?.query?.service?.toString() || "";
   if (service !== "git-upload-pack" && service !== "git-receive-pack") {
-    err(`Invalid git service: ${service}`);
-    return;
+    return err(`${service} is an invalid git service`);
   }
-  let response;
-  try {
-    response = await getGitBackend(service, true, req.body);
-  } catch (e) {
-    err(e.message);
-    return;
+  const response = await execPackService(service).catch(err);
+  if (response) {
+    res.setHeader("content-type", `application/x-${service}-advertisement`);
+    log.info(`Sending ${response.length} bytes of ref info`);
+    res.send(response);
   }
-  log.info(`Successfully got ${response.length} bytes of ref info`);
-  const contentType = "application/x-" + service + "-advertisement";
-  log.info(`setting content-type header to ${contentType}`);
-  res.setHeader("content-type", contentType);
-  res.send(response);
 };
 
 export const pack = async (req, res, _): Promise<void> => {
-  const err = (e: string): void => {
-    log.warn(`Git backend failure: ${e}`);
-    res.status(500).send(e);
-    return;
-  };
+  const err = getErrHandler(res);
   const service = req.path.split("/").pop();
   if (service !== "git-upload-pack" && service !== "git-receive-pack") {
-    err(`Invalid git service: ${service}`);
-    return;
+    return err(`${service} is an invalid git service`);
   }
-  log.info(`Activating git backend for ${req.body?.length} bytes posted to ${req.path}`);
-
-  let response;
-  try {
-    response = await getGitBackend(service, false, req.body);
-  } catch (e) {
-    err(e.message);
-    return;
+  const response = await execPackService(service, req.body).catch(err);
+  if (response) {
+    log.info(`Sending ${response.length} bytes of pack response`);
+    res.send(response);
   }
-
-  log.info(`Successfully got ${response.length} bytes of pack response`);
-  res.send(response);
 };
