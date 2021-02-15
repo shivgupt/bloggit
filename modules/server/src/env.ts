7596695eb201c16f702ac2c7b940124c1ea30eef
bohendo <bohendo@pm.me>
2021-02-15 20:01:31 +0530

    add BLOG_MIRROR_KEY env var to server

--- modules/server/src/env.ts
+++ modules/server/src/env.ts
@@ -1,11 +1,13 @@
 import path from "path";
 
 export type Env = {
-  authUsername: string;
   authPassword: string;
+  authUsername: string;
   contentDir: string;
   defaultBranch: string;
   logLevel: string;
+  mirrorKey: string;
+  mirrorUrl: string;
   port: number;
   prodMode: boolean;
 }
@@ -16,6 +18,8 @@ export const env: Env = {
   contentDir: path.normalize(process?.env?.BLOG_INTERNAL_CONTENT_DIR || "/blog-content.git"),
   defaultBranch: process?.env?.BLOG_DEFAULT_BRANCH || "main",
   logLevel: process?.env?.BLOG_LOG_LEVEL || "info",
+  mirrorKey: process?.env?.BLOG_MIRROR_KEY || "",
+  mirrorUrl: process?.env?.BLOG_MIRROR_URL || "",
   port: parseInt(process?.env?.BLOG_PORT || "8080", 10),
   prodMode: process?.env?.BLOG_PROD === "true",
 };
