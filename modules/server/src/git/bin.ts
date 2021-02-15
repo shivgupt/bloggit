5cb491c08198b3f82476ad61f57461250b8c61bd
bohendo <bohendo@pm.me>
2021-02-15 13:18:00 +0530

    dissolve backend child stream

--- modules/server/src/git/bin.ts
+++ modules/server/src/git/bin.ts
@@ -1,8 +1,9 @@
 import { spawn } from "child_process";
-import { Duplex } from "stream";
 
 import { env } from "../env";
 import { logger } from "../utils";
+
+import { bufferToStream, streamToBuffer } from "./utils";
 
 const log = logger.child({ module: "GitBackend" });
 
@@ -20,68 +21,12 @@ export const getGitBackend = async (
   log.info(`GitBackend(${service}, ${info}, payload.length=${payload ? payload.length : 0})`);
 
   return new Promise((resolve, reject) => {
-
     let output = Buffer.from([]);
-
     const args = [ "--stateless-rpc" ];
-    if (info) {
-      args.push("--advertise-refs");
-    }
+    if (info) args.push("--advertise-refs");
     args.push(env.contentDir);
 
-    ////////////////////////////////////////
-
-    let childStream: Duplex;
-    let inputReady: number;
-
-    const spawnChild = (inputBuf?: Buffer): void => {
-      log.info(`Creating child stream`);
-      let needsPktFlush: boolean;
-      let childBuffer = inputBuf;
-
-      childStream = new Duplex();
-      childStream._read = (n?: number): void => {
-        log.info(`reading ${n || 0} chars from child`);
-        if (childBuffer) {
-          log.debug(`pushing childBuffer to childStream`);
-          childStream.push(childBuffer);
-          childBuffer = null;
-        }
-      };
-      childStream._write = (
-        buf: string | Buffer,
-        enc: string,
-        next: (err?: Error) => void,
-      ): void => {
-        log.info(`writing ${buf ? buf.toString("utf8").length : 0} chars to child`);
-        if (buf.toString() === "0000") { // dont send terminate signal until childStream is finished
-          needsPktFlush = true;
-        } else {
-          log.debug(`Pushing ${buf ? buf.toString("utf8").length : 0} chars to output`);
-          output = Buffer.concat([output, Buffer.from(buf)]);
-        }
-        if (inputReady) {
-          log.debug(`Input is ready, activating child._write callback`);
-          next(null);
-        }
-      };
-      childStream.on("finish", (): void => {
-        log.debug(`childStream is finished, writing terminate signal to output`);
-        if (needsPktFlush) {
-          output = Buffer.concat([output, Buffer.from("0000")]);
-        }
-        log.debug(`Resolving promise directly`);
-        resolve(output);
-      });
-
-      if (info) {
-        const flag = "# service=" + service + "\n";
-        const n = (4 + flag.length).toString(16);
-        const toPush = Array(4 - n.length + 1).join("0") + n + flag + "0000";
-        log.debug(`Pushing ${toPush.length} chars of info data to output`);
-        output = Buffer.concat([output, Buffer.from(toPush)]);
-      }
-
+    const spawnChild = async (inputBuf?: Buffer): Promise<void> => {
       log.info(`Spawning: ${service} ${args.toString().split(",").join(" ")}`);
       const ps = spawn(service, args);
       ps.on("error", (e) => log.error(`${service} failed to launch: ${e}`));
@@ -89,16 +34,25 @@ export const getGitBackend = async (
       ps.stdout.on("data", out => log.debug(`${service} sent ${out.length} chars to stdout`));
       ps.stderr.on("data", err => log.error(`${service} sent ${err.length} chars to stderr`));
       ps.stdin.on("data", out => log.debug(`${service} got ${out.length} chars in stdin`));
-      ps.stdout.pipe(childStream).pipe(ps.stdin);
+      if (inputBuf) {
+        bufferToStream(inputBuf).pipe(ps.stdin);
+      }
+      const outputBuf = await streamToBuffer(ps.stdout);
+      output = Buffer.concat([output, outputBuf]);
+      resolve(output);
     };
 
     if (info) {
-      inputReady = 16384;
+      const flag = "# service=" + service + "\n";
+      const n = (4 + flag.length).toString(16);
+      const toPush = Array(4 - n.length + 1).join("0") + n + flag + "0000";
+      log.debug(`Pushing ${toPush.length} chars of info data to output`);
+      output = Buffer.concat([output, Buffer.from(toPush)]);
       spawnChild();
+
     } else if (payload && payload.length > 0) {
       if (regex[service].exec(payload.slice(0,512).toString("utf8"))) {
         log.debug(`Got regex match on ${service}'s input buffer, spawning child`);
-        inputReady = 16384;
         spawnChild(payload);
       } else {
         reject(new Error("unrecognized input"));
