2b60de5d10cfaa2ef3808f4f9b492b648900941b
bohendo <bohendo@pm.me>
2021-02-19 16:03:11 +0530

    fix ipfs to handle images

--- modules/server/src/ipfs.ts
+++ modules/server/src/ipfs.ts
@@ -19,35 +19,54 @@ const ipfs = ipfsClient({
 
 ipfsRouter.get("/*", async (req, res, _next): Promise<any> => {
   const path = `/ipfs/${req.path.replace(/^\//, "")}`;
-  log.info(`Getting path ${path} via ${req.method} to IPFS daemon`);
-  let output = "";
+  log.info(`${req.method}-ing path ${path}`);
+  const content = [];
   const list = [];
   try {
     for await (const chunk of ipfs.ls(path)) {
       list.push(chunk.name);
     }
+    log.debug(`Got list of ${list.length} files: ${list}`);
     for await (const chunk of ipfs.cat(path)) {
-      output += arrToString(chunk);
+      content.push(chunk);
     }
   } catch (e) {
     if (e.message === "this dag node is a directory") {
-      log.info(`Got list of ${list.length} file from ${path}`);
+      log.info(`Returning list of ${list.length} files from given directory`);
       return res.status(200).json(list);
     }
-    log.warn(e.message);
+    log.error(e);
     return res.status(500).send(e.message);
   }
-  log.info(`Got ${output.length} chars of contents from ${path}`);
-  return res.status(200).send(output);
+  try {
+    let text = "";
+    for (const chunk of content) {
+      text += arrToString(chunk);
+    }
+    log.info(`Returning ${text.length} chars of content`);
+    return res.status(200).send(text);
+  } catch (e) {
+    log.warn(e.message);
+    let bytes = Buffer.from([]);
+    for (const chunk of content) {
+      bytes = Buffer.concat([bytes, chunk]);
+    }
+    log.info(`Returning ${bytes.length} bytes of content`);
+    return res.status(200).send(bytes);
+  }
 });
 
-ipfsRouter.post("/", async (req, res, _next): Promise<any> => {
-  log.info(`POSTing path ${req.path} w data "${req.body}"`);
+const upload = async (req, res, _next): Promise<any> => {
+  log.info(`${req.method}-ing path ${req.path} w ${req.body.length || 0} bytes of data`);
   try {
     const result = await ipfs.add(req.body, { pin: true });
-    log.info(result, "Added file to ipfs, result:");
+    log.info(`Added file to ipfs with CID: ${result.path}`);
     return res.send(`/ipfs/${result.path}`);
   } catch (e) {
+    log.error(e.message);
     return res.status(500).send(e.message);
   }
-});
+};
+
+ipfsRouter.put("/", upload);
+ipfsRouter.post("/", upload);
