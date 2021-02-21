cf95f756988de04b9ee734d6fc749fea09d7fbf0
bohendo <bohendo@pm.me>
2021-02-21 13:42:50 +0530

    clean up invalid timestamps in git/history

--- modules/server/src/git/history.ts
+++ modules/server/src/git/history.ts
@@ -7,22 +7,38 @@ import { gitOpts, resolveRef } from "./utils";
 
 const log = logger.child({ module: "GitHistory" });
 
-const toISO = (secs: number): string => (new Date(Math.round(secs * 1000))).toISOString();
+type HistoryResult = Array<{ commit: string; timestamp: string; }>;
+type DateString = string; // in ISO format
 
-type HistoryResult = Array<{
-  commit: string;
-  timestamp: string;
-}>;
+const toISO = (secs: number): DateString => (new Date(secs * 1000)).toISOString();
+
+// Timestamp MUST increase at every commit. If it doesn't, use the prev's timestamp instead
+const earliest = (current: number, _prev?: number): number => {
+  const now = Math.round(Date.now() / 1000); // all math is done w units of seconds instead of ms
+  const prev = _prev || now;
+  if (prev > now) {
+    log.error(`prev timestamp ${toISO(prev)} is ahead of current time ${toISO(now)}`);
+  }
+  if (current > prev) {
+    log.warn(`Invalid timestamp: ${toISO(current)} using prev time instead: ${toISO(prev)}`);
+    return prev;
+  }
+  return current;
+};
+
 
 // Return an array of all commits w/in which the given filepath was modified
 export const history = async (req, res, _): Promise<any> => {
   const filepath = req.path.replace(`/history/`, "");
   const output = [] as HistoryResult;
 
-  const latestCommit = await resolveRef(env.branch);
+  const latestCommit = await git.readCommit({
+    ...gitOpts,
+    oid: await resolveRef(env.branch),
+  });
   let latestBlob;
   try {
-    latestBlob = (await git.readBlob({ ...gitOpts, oid: latestCommit, filepath })).oid;
+    latestBlob = (await git.readBlob({ ...gitOpts, oid: latestCommit.oid, filepath })).oid;
   } catch (e) {
     log.warn(e.message);
     return res.status(200).json(output);
@@ -33,14 +49,15 @@ export const history = async (req, res, _): Promise<any> => {
 
   let newFilepath = filepath;
   let prevBlob = latestBlob;
-  let prevCommit = latestCommit;
+  let prevCommit = latestCommit.oid;
+  let prevTimestamp = earliest(latestCommit.commit.committer.timestamp);
   for (const commit of commits) {
     log.debug(`Checking commit from ${toISO(commit.commit.committer.timestamp)}`);
     try {
       const blob = await git.readBlob({ ...gitOpts, oid: commit.oid, filepath: newFilepath });
       if (blob.oid !== prevBlob) {
         log.info(`${newFilepath} was updated between ${prevCommit} and ${commit.oid}`);
-        output.push({ commit: prevCommit, timestamp: toISO(commit.commit.committer.timestamp) });
+        output.push({ commit: prevCommit, timestamp: toISO(prevTimestamp) });
       }
       prevBlob = blob.oid;
     } catch (e) {
@@ -60,7 +77,7 @@ export const history = async (req, res, _): Promise<any> => {
             newFilepathFlag = true;
             output.push({
               commit: prevCommit,
-              timestamp: toISO(commit.commit.committer.timestamp),
+              timestamp: toISO(prevTimestamp),
             });
           }
           return blob.oid;
@@ -72,6 +89,7 @@ export const history = async (req, res, _): Promise<any> => {
       }
     }
     prevCommit = commit.oid;
+    prevTimestamp = earliest(commit.commit.committer.timestamp, prevTimestamp);
   }
 
   return res.status(200).json(output);
