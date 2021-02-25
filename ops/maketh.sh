d583c7b0a0cdbc42c8fa099aff58033ba41c2546
bohendo <bohendo@pm.me>
2021-02-25 23:31:35 +0530

    maketh should source the local prod env vars

--- ops/maketh.sh
+++ ops/maketh.sh
@@ -18,6 +18,8 @@ if [[ -n "$REACT_APP_ENV" ]]
 then
   echo "Loading env vars from $REACT_APP_ENV:"
   tee "$root/modules/client/.env.production.local" < "$REACT_APP_ENV"
+  # shellcheck disable=SC1090
+  source "$root/modules/client/.env.production.local"
 fi
 
 docker run \
