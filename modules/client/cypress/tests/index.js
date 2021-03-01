331c390f7f1300e702507c4de8de7ee19ced7d51
bohendo <bohendo@pm.me>
2021-03-01 22:48:16 +0530

    add cypress tests for browsing post history

--- modules/client/cypress/tests/index.js
+++ modules/client/cypress/tests/index.js
@@ -1,98 +1,58 @@
 /* global Cypress, cy */
 
-const my = {};
-
-my.authenticate = () => {
-  cy.visit(`${Cypress.env("baseUrl")}/admin`);
-  cy.get(`input[type="text"]`).clear().type("abc123");
-  cy.contains("button", /register/i).click();
-  cy.contains("div", /registered for admin access/i).should("exist")
-  cy.contains("button", /register/i).click();
-  cy.get(`a[href="/"]`).click();
-};
-
-my.toggleAdmin = () => {
-  cy.get(`button[aria-label="open drawer"]`).click();
-  cy.get(`input[type="checkbox"]`).click();
-  cy.get(`div[role="presentation"]`).click(10, 10);
-};
-
-my.editPost = (data) => {
-  for (const key of ["category", "path", "slug", "tags", "title", "tldr"]) {
-    if (typeof data[key] === "string") {
-      if (data[key].length > 0) {
-        cy.get(`input[name="${key}"]`).clear().type(data[key]);
-      } else {
-        cy.get(`input[name="${key}"]`).clear();
-      }
-    }
-  }
-  if (typeof data.content === "string") {
-    if (data.content.length > 0) {
-      cy.get(`textarea[data-testid="text-area"]`).clear().type(data.content);
-    } else {
-      cy.get(`textarea[data-testid="text-area"]`).clear();
-    }
-  }
-};
+const my = require("./utils");
 
 describe("Blog Client", () => {
   beforeEach(() => {
-    cy.visit(Cypress.env("baseUrl"));
     my.authenticate();
-    my.toggleAdmin();
+    my.enableAdmin();
   });
 
   it(`should create, edit, and delete a new post`, () => {
     const slug = "test";
+    my.createPost({
+      content: "Keep calm, this is only a test",
+      title: "Test Title",
+      tldr: "test tldr",
+      category: "test",
+      slug
+    });
+    const newTldr = "test2 tldr";
+    my.editPost({
+      content: "Keep calm, this is simply test number 2",
+      title: "Test Title II",
+      tldr: newTldr,
+      category: "test2",
+      slug,
+    });
+    my.archivePost(slug);
+    cy.contains(`p`, newTldr).should("not.exist");
+  });
 
-    // Create a new post
-    let content = "Keep calm, this is only a test";
-    let title = "Test Title";
-    let tldr = "test tldr";
-    let category = "test";
-    cy.get(`button#fab`).click();
-    my.editPost({ title, category, slug, tldr, content });
-    cy.get(`div#fab > button`).click();
-    cy.get(`button#fab-publish`).click();
-    cy.contains(`p`, content).should("exist");
-    cy.contains(`h2`, title).should("exist");
-    cy.location(`pathname`).should(`eq`, `/${slug}`)
-    cy.get(`a[href="/"]`).click();
-    cy.contains(`p`, tldr).should("exist");
-    cy.get(`button[aria-label="open drawer"]`).click();
-    cy.contains(`div[role="button"]`, category).should("exist");
-    cy.get(`div[role="presentation"]`).click(10, 10);
-
-    // Edit the post
-    content = "Keep calm, this is simply test number 2";
-    title = "Test Title II";
-    tldr = "test2 tldr";
-    category = "test2";
-    cy.get(`a[href="/${slug}"]`).click();
-    cy.get(`button#fab`).dblclick(); // TODO: why do we need to dblclick here?
-    my.editPost({ title, category, slug, tldr, content });
-    cy.get(`div#fab > button`).click();
-    cy.get(`button#fab-save`).click();
-    cy.contains(`p`, content).should("exist");
-    cy.contains(`h2`, title).should("exist");
-    cy.location(`pathname`).should(`eq`, `/${slug}`)
-    cy.get(`a[href="/"]`).click();
-    cy.contains(`p`, tldr).should("exist");
-    cy.get(`button[aria-label="open drawer"]`).click();
-    cy.contains(`div[role="button"]`, category).should("exist");
-    cy.get(`div[role="presentation"]`).click(10, 10);
-
-    // Archive the post
-    cy.visit(`${Cypress.env("baseUrl")}/admin`);
-    cy.contains(`span`, /posts/i).click();
-    cy.contains(`a[href="/${slug}"] ~ div > button`, /archive/i).click();
-    cy.contains(`span`, /posts/i).click();
-    cy.contains(`span`, /drafts/i).click();
-    cy.contains(`a[href="/${slug}"] ~ div > button`, /publish/i).should("exist");
-    cy.visit(Cypress.env("baseUrl"));
-    cy.contains(`p`, tldr).should("not.exist");
-
+  it(`should browse post history`, () => {
+    const slug = "test2";
+    const firstContent = "First content";
+    const secondContent = "Second content";
+    my.createPost({
+      content: firstContent,
+      title: "Test Title",
+      tldr: "test tldr",
+      category: "test",
+      slug
+    });
+    my.editPost({
+      content: secondContent,
+      title: "Test Title II",
+      tldr: "test2 tldr",
+      category: "test2",
+      slug,
+    });
+    cy.visit(`${Cypress.env("baseUrl")}/${slug}`);
+    cy.contains(`button`, /history/i).click();
+    cy.get(`ul > a`).first().click();
+    cy.location(`pathname`).should(`match`, /\/[a-f0-9]{8}\/[a-zA-Z0-0-]{1,}/)
+    cy.contains(`a`, /present/i).should("exist");
+    cy.contains(`p`, firstContent).should("exist");
   });
 
 });
---
+++ modules/client/cypress/tests/utils.js
@@ -0,0 +1,80 @@
+/* global Cypress, cy */
+
+const my = {};
+
+my.authenticate = () => {
+  cy.visit(`${Cypress.env("baseUrl")}/admin`);
+  cy.get(`input[type="text"]`).clear().type("abc123");
+  cy.contains("button", /register/i).click();
+  cy.contains("div", /registered for admin access/i).should("exist")
+  cy.contains("button", /register/i).click();
+  cy.get(`a[href="/"]`).click();
+};
+
+my.enableAdmin = () => {
+  cy.get(`button[aria-label="open drawer"]`).click();
+  cy.get(`input[type="checkbox"]`).click();
+  cy.get(`div[role="presentation"]`).first().click(10, 10);
+};
+
+my.enterPostData = (data) => {
+  for (const key of ["category", "path", "slug", "tags", "title", "tldr"]) {
+    if (typeof data[key] === "string") {
+      if (data[key].length > 0) {
+        cy.get(`input[name="${key}"]`).clear().type(data[key]);
+      } else {
+        cy.get(`input[name="${key}"]`).clear();
+      }
+    }
+  }
+  if (typeof data.content === "string") {
+    if (data.content.length > 0) {
+      cy.get(`textarea[data-testid="text-area"]`).clear().type(data.content);
+    } else {
+      cy.get(`textarea[data-testid="text-area"]`).clear();
+    }
+  }
+};
+
+my.createPost = (data) => {
+  cy.get(`button#fab`).click();
+  my.enterPostData(data);
+  cy.get(`div#fab > button`).click();
+  cy.get(`button#fab-publish`).click();
+  cy.contains(`p`, data.content).should("exist");
+  cy.contains(`h2`, data.title).should("exist");
+  cy.location(`pathname`).should(`eq`, `/${data.slug}`)
+  cy.get(`a[href="/"]`).click();
+  cy.contains(`p`, data.tldr).should("exist");
+  cy.get(`button[aria-label="open drawer"]`).click();
+  cy.contains(`div[role="button"]`, data.category).should("exist");
+  cy.get(`div[role="presentation"]`).click(10, 10);
+};
+
+my.editPost = (data) => {
+  cy.get(`a[href="/${data.slug}"]`).click();
+  cy.get(`button#fab`).dblclick(); // TODO: why do we need to dblclick here?
+  my.enterPostData(data);
+  cy.get(`div#fab > button`).click();
+  cy.get(`button#fab-save`).click();
+  cy.contains(`p`, data.content).should("exist");
+  cy.contains(`h2`, data.title).should("exist");
+  cy.location(`pathname`).should(`eq`, `/${data.slug}`)
+  cy.get(`a[href="/"]`).click();
+  cy.contains(`p`, data.tldr).should("exist");
+  cy.get(`button[aria-label="open drawer"]`).click();
+  cy.contains(`div[role="button"]`, data.category).should("exist");
+  cy.get(`div[role="presentation"]`).click(10, 10);
+};
+
+my.archivePost = (slug) => {
+  cy.visit(`${Cypress.env("baseUrl")}/admin`);
+  cy.contains(`span`, /posts/i).click();
+  cy.contains(`a[href="/${slug}"] ~ div > button`, /archive/i).click();
+  cy.contains(`span`, /posts/i).click();
+  cy.contains(`span`, /drafts/i).click();
+  cy.contains(`a[href="/${slug}"] ~ div > button`, /publish/i).should("exist");
+  cy.visit(Cypress.env("baseUrl"));
+};
+
+module.exports = my
