3bd8f9a59e2ac81a9175078fac63f63c9cbf6cbb
bohendo <bohendo@pm.me>
2021-03-03 12:47:07 +0530

    make ipfs pin deletion return an ipfs path

--- modules/server/src/ipfs/delete.ts
+++ modules/server/src/ipfs/delete.ts
@@ -1,4 +1,6 @@
 import { ipfs } from "./utils";
 
-export const rmPin = async (cid: string): Promise<string> =>
-  ipfs.pin.rm(cid, { recursive: true });
+export const rmPin = async (cid: string): Promise<string> => {
+  const unpinnedCid = await ipfs.pin.rm(cid, { recursive: true });
+  return `/ipfs/${unpinnedCid.toString()}`;
+};
