3a1827f732bd31cc5d5a3e8e6bcfa9466336215c
bohendo <bohendo@pm.me>
2021-03-05 23:37:18 +0530

    don't abort cypress tests after first failure

--- modules/client/cypress/support/index.js
+++ modules/client/cypress/support/index.js
@@ -49,29 +49,3 @@ Cypress.on("fail", error => {
   // still need to throw the error so tests wont be marked as a pass
   throw error;
 });
-
-////////////////////////////////////////
-// To exit tests after first failure, the following code was copy/pasted from:
-// https://github.com/cypress-io/cypress/issues/518#issuecomment-552382781
-
-function abortEarly() {
-  if (this.currentTest.state === "failed") {
-    return cy.task("shouldSkip", true);
-  }
-  cy.task("shouldSkip").then(value => {
-    if (value) this.skip();
-  });
-}
-
-beforeEach(abortEarly);
-afterEach(abortEarly);
-before(() => {
-  if (Cypress.browser.isHeaded) {
-    // Reset the shouldSkip flag at the start of a run, so that it
-    //  doesn't carry over into subsequent runs.
-    // Do this only for headed runs because in headless runs,
-    //  the `before` hook is executed for each spec file.
-    cy.task("resetShouldSkipFlag");
-  }
-});
-////////////////////////////////////////
