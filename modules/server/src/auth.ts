e603aae4059da907d0ee6c634cc9e9fc7437d378
bohendo <bohendo@pm.me>
2021-03-14 15:06:08 +0530

    add server log re why auth failed

--- modules/server/src/auth.ts
+++ modules/server/src/auth.ts
@@ -7,7 +7,7 @@ const log = logger.child({ module: "AuthRouter" });
 
 export const authRouter = express.Router();
 
-const authHeader = "authorization";
+const authHeaderKey = "authorization";
 const authType = "Basic";
 const encodedToken = Buffer.from(`${env.authUsername}:${env.authPassword}`).toString("base64");
 
@@ -16,12 +16,36 @@ const restrictedPaths = ["/git/edit", "/git/git-receive-pack", "/ipfs"];
 
 authRouter.use((req, res, next) => {
   if (restrictedPaths.includes(req.path) || restrictedMethods.includes(req.method)) {
-    if (!req.headers[authHeader] || req.headers[authHeader] !== `${authType} ${encodedToken}`) {
-      log.info(`Failed to authenticate request for ${req.path}`);
+    const authHeader = req.headers[authHeaderKey];
+    if (!req.headers[authHeaderKey] || authHeader !== `${authType} ${encodedToken}`) {
       if (req.path === "/git") {
         res.removeHeader("www-authenticate"); // prevents browser from popping up a login window.
       } else {
         res.setHeader("www-authenticate", authType);
+      }
+      // Print a helpful error message describing why auth failed
+      const prefix = `Failed to auth ${req.method} to ${req.path}`;
+      if (!authHeader) {
+        log.warn(`${prefix}, no ${authHeaderKey} header provided.`);
+      } else if (!authHeader.includes(" ")) {
+        log.warn(`${prefix}, invalid auth header format. Got: "${authHeader}"`);
+      } else if (!authHeader.startsWith(`${authType} `)) {
+        log.warn(`${prefix}, invalid auth type. Got ${authHeader.split(" ")[0]} (!== ${authType})`);
+      } else {
+        const givenToken = Buffer.from(authHeader.split(" ")[1]!, "base64").toString("utf8");
+        if (!givenToken || !givenToken.includes(":")) {
+          log.warn(`${prefix}, invalid token format. Got: "${givenToken}"`);
+        } else {
+          const givenUser = givenToken.split(":")[0];
+          const givenPassword = givenToken.split(":")[1];
+          if (env.authUsername !== givenUser) {
+            log.warn(`${prefix}, invalid username. Got: ${givenUser} (!= ${env.authUsername})`);
+          } else if (env.authPassword !== givenPassword)  {
+            log.warn(`${prefix}, invalid password. Got: ${givenPassword} (!= ${env.authPassword})`);
+          } else {
+            log.warn(`${prefix}, unknown error verifying token: ${givenToken}`);
+          }
+        }
       }
       res.status(401).send("Unauthorized");
     } else {
