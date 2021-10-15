59e7f320a97c5aafe0c024e74e9532ab522915d6
bohendo <bohendo@pm.me>
2021-10-15 11:08:59 -0400

    update version check & deploy scripts

--- ops/version-check.sh
+++ ops/version-check.sh
@@ -1,29 +1,79 @@
 #!/usr/bin/env bash
 set -e
 
-root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
-project=$(grep -m 1 '"name":' "$root/package.json" | cut -d '"' -f 4)
+root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)
 
-# Format string describing how each line looks
-function format {
- awk '{printf("| %-32s|%8s  ->  %-8s|\n", $1, $3, $4)}'
+do_not_upgrade=''
+
+if [[ -n "$do_not_upgrade" ]]
+then
+  # Create the sed command to remove any ignored packages
+  filter_args=""
+  for ignored in $do_not_upgrade
+  do filter_args="$filter_args\<${ignored//\//\\/}\>\|"
+  done
+  filter_cmd="/\(${filter_args::-2}\)[^-]/d"
+else
+  filter_cmd='s/ / /' # no-op
+fi
+#echo "filtering packages to ignore with sed cmd '$filter_cmd'"
+
+########################################
+## Define Helper Functions
+
+function printOutdated {
+  package="$1"
+  echo "===== $package"
+
+  cd "$(dirname "$package")" || exit 1
+
+  davidRes=$("$root/node_modules/.bin/david" || true)
+
+  if [[ "$davidRes" == "All dependencies up to date" ]]
+  then
+    echo "$davidRes"
+  else
+    # shellcheck disable=SC2016
+    format='{printf("| %-48s|%8s  ->  %-12s|\n", $1, $2, $3)}'
+    table=$(echo "$davidRes" \
+      | grep "│" \
+      | grep -v "Name" \
+      | tr '│' '|' \
+      | sed 's/|\+/|/g' \
+      | tr -d ' \t' \
+      | cut -d "|" -f 2-4 \
+      | tr '|' ' ' \
+      | awk '$3 != $4' \
+      | awk "$format" \
+      | sed "$filter_cmd" \
+    )
+    upgrades=$(echo "$davidRes" \
+      | grep "npm install" \
+      | cut -d " " -f 4- \
+      | tr ' ' '\n' \
+      | sed "$filter_cmd" \
+      | tr '\n' ' ' \
+      | sed 's/\(^ \+\| \+$\)//g' \
+    )
+
+    if [[ -z "$table" ]]
+    then echo "All packages are up to date"
+    else
+      echo "$table"
+      echo
+      # shellcheck disable=SC2016
+      echo "for f in $upgrades; do bash ops/upgrade-package.sh "'"${f%@*}" "${f##*@}"'"; done"
+    fi
+  fi
+
+  echo
+  cd - > /dev/null
 }
 
-echo "===== Package: $project/package.json"
-npm outdated -D | tail -n +2 | awk '$3 != $4' | format
-echo
+########################################
+## Execute
 
-for package in modules/*/package.json
-do
-  cd "$(dirname "$package")" || exit 1
-  echo "===== Package: $project/$(dirname "$package")/package.json"
-  mv package.json package.json.backup
-  sed "/@$project/d" < package.json.backup > package.json
-  npm outdated | tail -n +2 | awk '$3 != $4' | format
-  echo "-----"
-  npm outdated -D | tail -n +2 | awk '$3 != $4' | format
-  rm package.json
-  mv package.json.backup package.json
-  cd "$root" || exit 1
-  echo
+printOutdated "package.json"
+find modules/ -maxdepth 2 -type f -name "package.json" | while read -r package
+do printOutdated "$package"
 done
