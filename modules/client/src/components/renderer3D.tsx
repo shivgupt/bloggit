6b4b34f2b87b3c53f84c652b98ab370e6c349480
shivhendo <shivhendo@proton.me>
2022-08-08 16:50:32 -0400

    rm commented code

--- modules/client/src/components/renderer3D.tsx
+++ modules/client/src/components/renderer3D.tsx
@@ -8,18 +8,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 export const Renderer3D = (src: any) => {
   console.log(src)
   let srcURL = `http://${window.location.host}${src.src}`;
-  /*
-  const loader = new GLTFLoader();
-  loader.load("https://arweave.net/8kWTyLavFfivf3uhtyWiCa8VQ6M8R7XHP3ed991NrLA", (gltf) => {
-    console.log(gltf);
-  }, undefined, (error) => {
-    console.log( error );
-  });
-  */
 
-  const arweaveSrc = "https://arweave.net/8kWTyLavFfivf3uhtyWiCa8VQ6M8R7XHP3ed991NrLA";
   const gltf = useLoader(GLTFLoader, srcURL);
-    console.log("This is the gltf", gltf);
 
   return (
     <div style = {{height:"100vh", width:"100%"}}>
