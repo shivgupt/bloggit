174c6fccf4ab1a0eabac8186784c57538e50764b
shivani <gupta.shivani254@gmail.com>
2022-08-12 14:03:59 -0400

    change marker box -> plane

--- modules/client/src/components/VRHouseTour.tsx
+++ modules/client/src/components/VRHouseTour.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { VRCanvas, ARCanvas, useXR, useController, DefaultXRControllers, Interactive } from '@react-three/xr'
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader, useGraph, useFrame } from "@react-three/fiber";
-import { Box } from '@react-three/drei'
+import { Plane } from '@react-three/drei'
 import * as THREE from "three"
 
 export const VRHouseTour = () => {
@@ -48,9 +48,9 @@ export const VRHouseTour = () => {
       <VRCanvas frameloop="demand" dpr={[1, 1.5]} shadows camera={{ near: 0.1, far: 100, fov: 75 }}>
         <ambientLight intensity={0.5} />
         <Interactive onSqueeze={(event) => move(event)}>
-          <Box position={marker}>
+          <Plane position={marker}>
             <meshBasicMaterial color="hotpink" />
-          </Box>
+          </Plane>
           <mesh>
             <primitive object={glb.scene} scale={1} />
           </mesh>
