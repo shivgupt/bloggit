deaa160ae785ffd16b8ea216a78c4ca71884e120
bohendo <bohendo@pm.me>
2022-10-17 19:03:28 -0400

    fix urbit container

--- modules/urbit/entry.sh
+++ modules/urbit/entry.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/nix/store/fa138pjfv8b4sg5l80ls55ynz9g4s3p9-bash-interactive-5.1-p16/bin/bash
 
 DATADIR="${DATADIR:-/root/data}"
 PORT="${PORT:-80}"
@@ -13,5 +13,5 @@ echo "- urbit executable: $(ls -l $(which urbit))"
 echo
 
 cd "$DATADIR" || exit 1
-echo /bin/urbit --http-port="$PORT" "$URBIT_NAME"
-exec /bin/urbit --http-port="$PORT" "$URBIT_NAME"
+echo start-urbit --http-port="$PORT" "$URBIT_NAME"
+exec start-urbit --http-port="$PORT" "$URBIT_NAME"
