a693ca1afd6ec87de3ceffbfdcd751b79968baf4
bohendo <bohendo@pm.me>
2022-10-17 18:44:37 -0400

    tweak urbit entry

--- modules/urbit/entry.sh
+++ modules/urbit/entry.sh
@@ -8,6 +8,10 @@ echo "Running urbit in env"
 echo "- DATADIR: $DATADIR"
 echo "- PORT: $PORT"
 echo "- URBIT_NAME: $URBIT_NAME"
+echo "- urbit in path: $(which urbit)"
+echo "- urbit executable: $(ls -l $(which urbit))"
+echo
 
 cd "$DATADIR" || exit 1
-urbit --http-port="$PORT" "$URBIT_NAME"
+echo /bin/urbit --http-port="$PORT" "$URBIT_NAME"
+exec /bin/urbit --http-port="$PORT" "$URBIT_NAME"
