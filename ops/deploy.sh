d6492d5d8097f43ddfaf485c49e41818c07d96c7
bohendo <bohendo@pm.me>
2022-11-23 16:24:37 -0500

    add guardrail to deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -3,14 +3,13 @@ set -e
 
 server="$1"
 user="${SERVER_USER:-admin}"
-key="$SSH_KEY:-$HOME/.ssh/id_ed25519"
 
 if [[ -z "$server" || -n "$2" ]]
 then echo "Please provide the server ip as the first & only arg" && exit 1
 fi
 
-if [[ ! -f "$key" ]]
-then echo "Please set the SSH_KEY env var to the path to your ssh key" && exit 1
+if [[ -n "$(git diff origin/prod)" ]]
+then echo "Aborting: Make sure your branch is up to date with origin/prod" && exit 1
 fi
 
 echo "Preparing to deploy prod branch at commit $(git rev-parse HEAD | head -c 8) to server at $server"
