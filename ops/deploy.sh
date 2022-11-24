59bec9dbb1dcfabab5ed1a7dbcd4631c9157023c
bohendo <bohendo@pm.me>
2022-11-23 19:14:38 -0500

    allow put http methods through proxy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -8,17 +8,15 @@ if [[ -z "$server" || -n "$2" ]]
 then echo "Please provide the server ip as the first & only arg" && exit 1
 fi
 
-if [[ -n "$(git diff origin/prod)" ]]
-then echo "Aborting: Make sure your branch is up to date with origin/prod" && exit 1
-fi
-
 echo "Preparing to deploy prod branch at commit $(git rev-parse HEAD | head -c 8) to server at $server"
 sleep 2
 
 git push ssh://$user@$server/home/$user/bloggit prod
 
-ssh $user@$sever -i "$key" <<EOF
+ssh $user@$server <<EOF
 cd bloggit
 git reset --hard prod
+make reset-images
+make all
 make restart
 EOF
