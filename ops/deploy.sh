5ba8af231efe3018bbcb5236b6c7a89eb23f4779
bohendo <bohendo@pm.me>
2022-11-30 22:32:09 -0500

    fix deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -5,6 +5,7 @@ root=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
 user="${SERVER_USER:-admin}"
 ipFile="$root/ops/prod/PROD_SERVER_IP_ADDRESS"
 commit="$(git rev-parse HEAD | head -c 8)"
+branch="$(git branch --show-current)"
 
 if [[ ! -f "$ipFile" ]]
 then echo "Please provide a valid ip address in a file at: $ipFile" && exit 1
@@ -14,10 +15,14 @@ ipAddress="$(cat "$ipFile")"
 remote="ssh://$user@$ipAddress/home/$user/bloggit"
 
 echo "Preparing to deploy commit $commit to server at $ipAddress"
-sleep 2
+sleep 2 # last chance to ctrl-c
 
-git push $remote $commit
+echo
+echo "Pushing branch $branch to server at $ipAddress"
+git push $remote $branch
 
+echo
+echo "Restarting bloggit"
 ssh $user@$ipAddress <<EOF
 cd bloggit
 git reset --hard $commit
@@ -25,3 +30,6 @@ make reset-images
 make all
 make restart
 EOF
+
+echo
+echo "Done!"
